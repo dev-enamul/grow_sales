049a840ea917afba28f9b0010a1c9bff8398a09c
Md Enamul Haque <enamul807849@gmail.com>
2025-02-03 15:10:16 +0600

    profile picture update api

--- app/Http/Controllers/User/ProfileUpdateController.php
+++ app/Http/Controllers/User/ProfileUpdateController.php
@@ -11,27 +11,42 @@ use Illuminate\Support\Facades\Auth;
 
 class ProfileUpdateController extends Controller
 {
-    public function profile_picture(Request $request){
+    public function profile_picture(Request $request)
+    { 
         $request->validate([
             'uuid' => 'required|uuid',
             'profile_image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
-        try{
+        try { 
             $uuid = $request->uuid;
             $authUser = Auth::user();
-            $user = User::where('uuid',$uuid)->where('company_id',$authUser->company_id)->first(); 
-            if(!$user){
-                error_response(null,404,"User not found");
+ 
+            $user = User::where('uuid', $uuid)
+                        ->where('company_id', $authUser->company_id)
+                        ->first(); 
+ 
+            if (!$user) {
+                return error_response(null, 404, "User not found");
             }
-            $user->update([
-                'profile_image' => $request->file('profile_image') ? $request->file('profile_image')->store('profile_images', 'public') : null,
-            ]); 
-            return success_response(null,"Profile picture updated successfully");
-        }catch(Exception $e){
-            return error_response($e->getMessage(),500); 
+ 
+            if ($request->hasFile('profile_image')) {
+                $image = $request->file('profile_image'); 
+                $imagePath = $image->store('profile_images', 'public'); 
+                $user->update([
+                    'profile_image' => $imagePath,
+                ]);
+            } else { 
+                $user->update([
+                    'profile_image' => null,
+                ]);
+            } 
+            return success_response(null, "Profile picture updated successfully"); 
+        } catch (Exception $e) { 
+            return error_response($e->getMessage(), 500);
         }
     }
+
 
     public function bio(Request $request){
        try{
